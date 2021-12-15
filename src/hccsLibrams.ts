5483dcf87ed8c7254a0b2fbf59616e6876b1c020
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2021-12-15 15:48:20 -0600

    make libram burning work?

--- src/hccsLibrams.ts
+++ src/hccsLibrams.ts
@@ -1,24 +1,36 @@
-import { availableAmount, print, useSkill } from "kolmafia";
-import { $item, $skill } from "libram";
-import { testDone } from "./hccs";
+import { availableAmount, mpCost, myMaxmp, myMp, useSkill } from "kolmafia";
+import { $item, $skill, get } from "libram";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function castBestLibram() {
-  if (availableAmount($item`green candy heart`) < 1 && !testDone(5)) {
+function nextLibramCost() {
+  return mpCost($skill`Summon BRICKOs`);
+}
+
+export function castBestLibram() {
+  if (get("_brickoEyeSummons") < 3) {
+    useSkill($skill`Summon BRICKOs`);
+  } else if (
+    availableAmount($item`green candy heart`) < 1 &&
+    !get("csServicesPerformed").includes("Breed More Collies")
+  ) {
+    // need an alternative to testDone(), since that hits council.php
     useSkill($skill`Summon Candy Heart`);
-  } else if (availableAmount($item`love song of icy revenge`) < 2 && !testDone(5)) {
-    useSkill($skill`Summon Love Song`);
-  } else if (availableAmount($item`lavender candy heart`) < 1 && !testDone(9)) {
+  } else if (
+    availableAmount($item`lavender candy heart`) < 1 &&
+    !get("csServicesPerformed").includes("Make Margaritas")
+  ) {
     useSkill($skill`Summon Candy Heart`);
-  } else if (availableAmount($item`love song of icy revenge`) < 3 && !testDone(5)) {
+  } else if (
+    availableAmount($item`love song of icy revenge`) < 3 &&
+    !get("csServicesPerformed").includes("Breed More Collies")
+  ) {
     useSkill($skill`Summon Love Song`);
   } else {
     useSkill($skill`Summon Party Favor`);
   }
 }
-export function libramCast() {
-  /*while (myMp() / myMaxmp() > 0.2 && nextLibramCost() <= myMp()) {
+
+export function libramBurn() {
+  while (myMp() / myMaxmp() > 0.2 && nextLibramCost() <= myMp()) {
     castBestLibram();
-  } */
-  print("nevermind");
+  }
 }
